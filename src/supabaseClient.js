72d83538aa00185ebae48b959155149787168eda
Lazzy_Tarun <crazybot0002@gmail.com>
2026-06-03 20:46:00 +0530

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Paste your copied credentials right here
-const supabaseUrl = 'https://dxhydtntyzvxukakxdup.supabase.co/rest/v1/'; 
+const supabaseUrl = 'https://dxhydtntyzvxukakxdup.supabase.co'; 
 const supabaseAnonKey = 'sb_publishable_ZSNiCTK-FHEa3pi-71DFZA_eJSsKoNp'; // Paste the full copied key here
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
